e28914cfce811e1249d383e3313579d17ef1676d
behappycc <behappyccc@gmail.com>
2015-12-05 21:58:31 +0800

    TODO random graph

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from networkx import *
 import sys
+import math
 
 def readFile(filename):
     listData = []
@@ -19,11 +20,15 @@ def readFile(filename):
 
 def ErdosRenyi():
     n=5000
-    m=50
-
+    #G(n, m), G(n,p) m = c(n, 2) * p, c(5000, 2) * 0.01 = 124975
+    m = int(Cnr(5000, 2) * 0.01)
     G=gnm_random_graph(n,m)
     print 'random_graph value: ' + str(nx.average_clustering(G))
     #nx.draw(G)
+
+def Cnr(n, r):
+    f = math.factorial
+    return f(n) / f(r) / f(n - r)
     
 def main():
     ErdosRenyi()
@@ -31,7 +36,7 @@ def main():
     readFile('p2p-Gnutella04.txt')
     readFile('Wiki-Vote.txt')
     readFile('WikiTalk.txt')
-    plt.show()
+    #plt.show()
 
 if __name__ == '__main__':
     main()
