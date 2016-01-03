7fb3b879413b0f98f9af9048a9576e2bcbbec394
behappycc <behappyccc@gmail.com>
2016-01-03 22:05:31 +0800

    TODO web

---
+++ final/ptt/web/Server.py
@@ -0,0 +1,153 @@
+# coding=UTF-8
+# python native module
+import urllib
+import threading
+import thread
+import argparse
+import os.path
+import json
+import pprint
+# tornado module
+import tornado.httpserver
+import tornado.httputil as httputil
+import tornado.ioloop
+import tornado.web
+from tornado.options import define, options
+from  tornado.escape import json_decode
+from  tornado.escape import json_encode
+# mongodb module
+import pymongo
+from pymongo import MongoClient
+#custom files
+import Pttuser as ptt
+import Topic as tp
+
+#localhost:8000/index
+#140.112.42.151
+DB_IP = "localhost"
+DB_PORT = 27017
+
+def main():
+    parser = argparse.ArgumentParser(description='ptt-clustering server')
+    parser.add_argument('-p', type=int, help='listening port for ptt-clustering server')
+    parser.add_argument('-b', metavar='BOARD_NAME', help='Board name', required=True)
+    args = parser.parse_args()
+    global board
+    port = args.p
+    board = args.b
+    
+    print("Server starting......")
+
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(port)
+
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop()
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", IndexHandler),
+            (r"/index", IndexHandler),
+            (r"/chart", ChartHandler),
+            (r"/user",UserHandler),
+            (r"/test",TestHandler),
+            (r"/testajax", AjaxHandler),
+        ]
+
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        pass
+        #return self.settings['db']
+
+class IndexHandler(BaseHandler):
+    def __init__(self, application, request, **kwargs):
+        self.popularEvents = {}
+        self.newEvents = {}
+        super(IndexHandler, self).__init__(application, request, **kwargs)
+
+    def get(self):
+        #list1Topic, list7Topic = tp.statisticTopic(board)
+        list7Topic = tp.statisticTopicFast(board)
+        self.popularEvents = tp.topicFreqDB(list7Topic)[:10]
+        self.newEvents = tp.recommendArticle(list7Topic, 5)[:7]
+       # self.newEvents = tp.randomChoice(tp.recommendArticle(list7Topic, 5), 5)
+        self.render("index.html", popularEvents = self.popularEvents, newEvents = self.newEvents)
+        #https://www.ptt.cc/bbs/Gossiping/M.1451802547.A.C20.html
+
+    def post(self):
+        json_obj = json_decode(self.request.body)
+        #print 'Post data received'
+        #for key in list(json_obj.keys()):
+        #   print 'key: %s , value: %s' % (key, json_obj[key])
+        print json_obj['key1']
+
+        # new dictionary
+        response_to_send = {}
+        #response_to_send['newkey'] = json_obj['key1']
+        response_to_send['topic'] = tp.topicFreqDB(tp.statisticSchoolTopic(board, json_obj['key1']))
+        #print('Response to return')
+        #pprint.pprint(response_to_send)
+
+        self.write(json.dumps(response_to_send))
+
+class ChartHandler(BaseHandler):
+    def get(self):
+        self.render("chart.html")
+
+class UserHandler(BaseHandler):
+    def __init__(self, application, request, **kwargs):
+        self.username = ''
+        self.records = {}
+        super(UserHandler, self).__init__(application, request, **kwargs)
+
+    def get(self):
+        self.render("user.html", username = self.username, records = self.records)
+
+    def post(self):
+        username = self.get_argument("username")
+        print username
+        records = ptt.queryUser('OtsukaAi', username)  
+        #records = ptt.queryUser('Gossiping', username)
+        self.render("user.html", username = username, records = records)
+
+class TestHandler(BaseHandler):
+    def get(self):
+        self.render("test.html")
+        
+class AjaxHandler(tornado.web.RequestHandler):
+    def get(self):
+        example_response = {}
+        example_response['name'] = 'example'
+        example_response['width'] = 1020
+
+        self.write(json.dumps(example_response))
+
+    def post(self):
+        json_obj = json_decode(self.request.body)
+        print('Post data received')
+
+        for key in list(json_obj.keys()):
+            print('key: %s , value: %s' % (key, json_obj[key]))
+
+        # new dictionary
+        response_to_send = {}
+        response_to_send['newkey'] = json_obj['key1']
+
+        print('Response to return')
+
+        pprint.pprint(response_to_send)
+
+        self.write(json.dumps(response_to_send))
+
+if __name__ == '__main__':
+    main()
---
+++ final/ptt/web/static/js/index.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+      $('.parallax').parallax();
+
+      $("#btn_Run").click(function(){
+        alert("hello fatty");
+      });
+
+      /*
+      $('#runajax').click(function (event) {
+
+                var valueForInput1 = $("#input1").val();
+                var valueForInput2 = $("#input2").val();
+
+                var data =
+                {
+                    key1: valueForInput1,
+                    key2: valueForInput2
+                };
+
+                var dataToSend = JSON.stringify(data);
+
+                $.ajax(
+                        {
+                            url: '/testajax',
+                            type: 'POST',
+                            data: dataToSend,
+
+                            success: function (jsonResponse) {
+                                var objresponse = JSON.parse(jsonResponse);
+                                console.log(objresponse['newkey']);
+
+                                $("#responsefield").text(objresponse['newkey']);
+
+                            },
+                            error: function () {
+                                $("#responsefield").text("Error to load api");
+
+                            }
+                        });
+
+                event.preventDefault();
+            });
+*/
+
+  var density = {
+    "台北": 9952.60,
+    "嘉義": 4512.66,
+    "新竹": 4151.27,
+    "基隆": 2809.27,
+    "台北": 1932.91,
+    "桃園": 1692.09,
+    "台中": 1229.62,
+    "彰化": 1201.65,
+    "高雄": 942.97,
+    "台南": 860.02,
+    "金門": 847.16,
+    "澎湖": 802.83,
+    "雲林": 545.57,
+    "連江縣": 435.21,
+    "新竹": 376.86,
+    "苗栗": 311.49,
+    "屏東": 305.03,
+    "嘉義": 275.18,
+    "宜蘭": 213.89,
+    "南投": 125.10,
+    "花蓮": 71.96,
+    "台東": 63.75
+  };
+  d3.json("static/json/county.json", function(topodata) {
+    var features = topojson.feature(topodata, topodata.objects.county).features;
+    var color = d3.scale.linear().domain([0,10000]).range(["#090","#f00"]);
+    var fisheye = d3.fisheye.circular().radius(100).distortion(2);
+    var prj = function(v) {
+      var ret = d3.geo.mercator().center([122,23.25]).scale(6000)(v);
+      var ret = fisheye({x:ret[0],y:ret[1]});
+      return [ret.x, ret.y];
+    };
+    var path = d3.geo.path().projection(prj);
+    for(idx=features.length - 1;idx>=0;idx--) features[idx].density = density[features[idx].properties.C_Name];
+    d3.select("svg").selectAll("path").data(features).enter().append("path");
+    function update() {
+      d3.select("svg").selectAll("path").attr({
+        "d": path,
+        "fill": function (d) { return color(d.density); }
+      }).on("mouseover", function(d) {
+        $("#name").text(d.properties.C_Name);
+        $("#density").text(d.density);
+        //console.log(d.properties.C_Name)
+        var data = {
+          key1: d.properties.C_Name,
+          key2: '2'
+        };
+        var dataToSend = JSON.stringify(data);
+        $.ajax(
+          {
+              url: '/index',
+              type: 'POST',
+              data: dataToSend,
+              success: function (jsonResponse) {
+                var objresponse = JSON.parse(jsonResponse);
+                console.log(objresponse['topic']);
+                $("#topic").text(objresponse['topic']);
+              },
+              error: function () {
+                $("#topic").text("Error to load api");
+                   }
+              });
+        });
+
+    }
+    d3.select("svg").on("mousemove", function() {
+      fisheye.focus(d3.mouse(this));
+      update();
+    });
+    update();
+  });
+});
